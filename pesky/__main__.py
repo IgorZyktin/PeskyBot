5c989421cdfd84a5f19cf75ef3924a2911706ac1
IgorZyktin <nicord@yandex.ru>
2022-04-11 20:38:26 +0300

    fix: import of loggers

--- pesky/__main__.py
+++ pesky/__main__.py
@@ -2,11 +2,15 @@
 """Entry point."""
 from aiogram import executor
 
+from pesky import infra
 from pesky.bot.dependencies import dp
+
+LOG = infra.get_logger(__name__)
 
 
 def main():
     """Entry point."""
+    LOG.info('Starting PeskyBot!')
     executor.start_polling(dp)
 
 
