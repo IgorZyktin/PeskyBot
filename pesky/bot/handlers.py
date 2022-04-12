3ea2d573ff9717428a6adcdc89d709099f767809
IgorZyktin <nicord@yandex.ru>
2022-04-12 21:17:31 +0300

    feat: moved error_bot_blocked to commons

--- pesky/bot/handlers.py
+++ pesky/bot/handlers.py
@@ -2,7 +2,6 @@
 """Message handlers.
 """
 from aiogram import types
-from aiogram.utils import exceptions
 
 from pesky import messages, infra
 from pesky.bot.dependencies import dp
@@ -14,11 +13,3 @@ LOG = infra.get_logger(__name__)
 async def cmd_help(message: types.Message):
     """Show list of known operations."""
     await message.answer(messages.MSG_HELP)
-
-
-@dp.errors_handler(exception=exceptions.BotBlocked)
-async def error_bot_blocked(update: types.Update,
-                            exception: exceptions.BotBlocked):
-    """Handle case when bot is blocked."""
-    LOG.warning(f'User blocked me, message=%s, exc=%s', update, exception)
-    return True
--- pesky/bot/handlers_common.py
+++ pesky/bot/handlers_common.py
@@ -2,11 +2,15 @@
 """Common handlers.
 """
 from aiogram import types
+from aiogram.utils import exceptions
 
+from pesky import infra
 from pesky.bot.dependencies import dp, user_middleware, database_middleware
 from pesky.domain import models
 from pesky.storage import operations
 from pesky.storage.database.database import Database
+
+LOG = infra.get_logger(__name__)
 
 
 @dp.message_handler(commands='start')
@@ -29,3 +33,11 @@ async def cmd_start(
         output += '\nРад знакомству!'
 
     await message.answer(output)
+
+
+@dp.errors_handler(exception=exceptions.BotBlocked)
+async def error_bot_blocked(update: types.Update,
+                            exception: exceptions.BotBlocked):
+    """Handle case when bot is blocked."""
+    LOG.warning(f'User blocked me, message=%s, exc=%s', update, exception)
+    return True
